1d22e9c8489515d2ca6ee5f865f32e60d5cfd53d
Aldy Rizky Putra <aldyerpedevs@gmail.com>
2024-04-16 02:21:25 +0000

    Refine Design and Structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
 function App() {
   return (
     <div>
-      <h1 className="main-title">ReactJS Profile Card by Aldy Rizky Putra</h1>
-      <div className="container">
-        <ProfileImage />
-        <ProfileDetails />
-        <ProfileSkills />
+      <h1 className="main-title">ReactJS Profile Cards by Aldy Rizky Putra</h1>
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          <SkillList />
+        </div>
       </div>
     </div>
   );
 }
 
-function ProfileImage() {
-  return (
-    <img className="profile-img" src="images/aldyrp.png" alt="Aldy Images" />
-  );
+function Avatar() {
+  return <img className="avatar" src="images/aldyrp.png" alt="Aldy Images" />;
 }
 
-function ProfileDetails() {
+function Intro() {
   return (
-    <div className="profile-details">
-      <h2>Aldy Rizky Putra</h2>
-      <p>
+    <div>
+      <h2 className="name">Aldy Rizky Putra</h2>
+      <p className="description">
         Freelance Front End Developer that always try to learn any programming
         language, and frameworks, especially for web development. IT Addict
         since Elementary School. Now, learning Laravel. Stated in Tangerang,
@@ -36,69 +36,50 @@ function ProfileDetails() {
   );
 }
 
-function ProfileSkills() {
+function SkillList() {
   return (
-    <div className="profile-skills">
-      <ul>
-        <SkillsItem
-          bgColor="#225577"
-          txtColor="#ffffff"
-          skillTitle="HTML & CSS"
-          skillRate="💪"
-        />
-        <SkillsItem
-          bgColor="#ff5757"
-          txtColor="#ffffff"
-          skillTitle="JavaScript"
-          skillRate="💪"
-        />
-        <SkillsItem
-          bgColor="#57ff57"
-          txtColor="#000000"
-          skillTitle="PHP"
-          skillRate="💪"
-        />
-        <SkillsItem
-          bgColor="#5757ff"
-          txtColor="#ffffff"
-          skillTitle="MySQL"
-          skillRate="💪"
-        />
-        <SkillsItem
-          bgColor="#7e8021"
-          txtColor="#ffffff"
-          skillTitle="ReactJS"
-          skillRate="👦"
-        />
-        <SkillsItem
-          bgColor="#339922"
-          txtColor="#ffffff"
-          skillTitle="Laravel"
-          skillRate="👦"
-        />
-        <SkillsItem
-          bgColor="#90ff69"
-          txtColor="#000000"
-          skillTitle="Git"
-          skillRate="💪"
-        />
-      </ul>
+    <div className="skill-list">
+      <Skill skill="HTML & CSS" rate="💪" color="#ff3377" />
+      <Skill skill="JavaScript" rate="💪" color="#33ff77" />
+      <Skill skill="PHP" rate="💪" color="#ff33ff" />
+      <Skill skill="MySQL" rate="💪" color="#cc69ff" />
+      <Skill skill="Git" rate="💪" color="#10ff99" />
+      <Skill
+        skill="NodeJS"
+        rate="
+👦"
+        color="#befa99"
+      />
+      <Skill
+        skill="ReactJS"
+        rate="
+👦"
+        color="#cafa00"
+      />
+      <Skill
+        skill="Laravel"
+        rate="
+👦"
+        color="#dada11"
+      />
     </div>
   );
 }
 
-function SkillsItem(props) {
+function Skill(props) {
   return (
-    <li style={{ backgroundColor: props.bgColor, color: props.txtColor }}>
-      <span>{props.skillTitle}</span>
-      <span>{props.skillRate}</span>
-    </li>
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.rate}</span>
+    </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
